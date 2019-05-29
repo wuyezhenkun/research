07628bd066bcfae8532f99e40fca2e2b9e26cf5f
WeiliangBMBL <liu.3540@osu.edu>
2019-05-29 11:02:06 -0400

    Update GSE.bash

--- GSE.bash
+++ GSE.bash
@@ -6,7 +6,7 @@
 # echo commands to stdout 
 set –x
 
-cd /pylon5/cc5fpcp/weiliang/BMBL/r/test_final_2 
+cd /pylon5/cc5fpcp/weiliang/BMBL/research/m_clust/1:0/
 
 python3 get_matrix.py 
 Rscript use_Mclust.R 
