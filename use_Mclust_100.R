c7ac62f33259350d4d53b671dd307918e6415e69
WeiliangBMBL <liu.3540@osu.edu>
2019-05-29 08:42:50 -0400

    Update use_Mclust_100.R

--- use_Mclust_100.R
+++ use_Mclust_100.R
@@ -1,1 +1,13 @@
+library(mclust)
 
+filename <- readline()
+
+data = read.table(filename,header = T)
+
+data1 = data[,2:16]
+
+m_clust <- Mclust(as.matrix(data1),G=1:100)
+
+print(m_clust$G)
+
+save.image(file = 'result.RData')
