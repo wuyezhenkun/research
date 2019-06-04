3ea60a7d56bdeccaa9e56337f9f37a9a4f9e4f15
WeiliangBMBL <liu.3540@osu.edu>
2019-06-04 18:47:33 -0400

    Update use_Mclust.R

--- use_Mclust.R
+++ use_Mclust.R
@@ -1,1 +1,14 @@
+library(mclust)
+
+filename <- commandArgs(trailingOnly = TRUE)
+
+data = read.table(filename,header = T)
+
+data1 = data[,2:16]
+
+m_clust <- Mclust(as.matrix(data1),G=1:50)
+
+print(m_clust$G)
+
+save.image(file = paste( filename,'.RData'))
 
